06c3dac1f24b56c4971037e91251b983d80f7f24
Zoe Ernst <zoe.ernst@contractor.hallmark.com>
2022-01-16 18:58:18 -0300

    modified favicons and title

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/geoda/">
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
